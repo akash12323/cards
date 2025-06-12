13d36190345479cbea12840c37dc745e910ff865
akash12323 <aakashgpt12321@gmail.com>
2025-06-12 23:53:40 +0530

    initial commit

---
+++ src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -0,0 +1,82 @@
+package com.eazybytes.cards.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.cards.constants.CardsConstants;
+import com.eazybytes.cards.dto.CardsDto;
+import com.eazybytes.cards.dto.ResponseDto;
+import com.eazybytes.cards.service.ICardsService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+/**
+ * @author Eazy Bytes
+ */
+
+@Tag(
+        name = "CRUD REST APIs for Cards in EazyBank",
+        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE card details"
+)
+@RestController
+@RequestMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+public class CardsController {
+
+    private ICardsService iCardsService;
+
+    public CardsController(ICardsService iCardsService) {
+		this.iCardsService = iCardsService;
+	}
+    
+    @PostMapping("/cards")
+    public ResponseEntity<ResponseDto> createCard(@RequestParam String mobileNumber) {
+        iCardsService.createCard(mobileNumber);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(new ResponseDto(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
+    }
+
+    @GetMapping("/cards")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam String mobileNumber) {
+        CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
+    }
+
+    @PutMapping("/cards")
+    public ResponseEntity<ResponseDto> updateCardDetails(@RequestBody CardsDto cardsDto) {
+        boolean isUpdated = iCardsService.updateCard(cardsDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(CardsConstants.STATUS_200, CardsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(CardsConstants.STATUS_417, CardsConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @DeleteMapping("/cards")
+    public ResponseEntity<ResponseDto> deleteCardDetails(@RequestParam String mobileNumber) {
+        boolean isDeleted = iCardsService.deleteCard(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(CardsConstants.STATUS_200, CardsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(CardsConstants.STATUS_417, CardsConstants.MESSAGE_417_DELETE));
+        }
+    }
+
+}
---
+++ src/main/java/com/eazybytes/cards/entity/Cards.java
@@ -0,0 +1,163 @@
+package com.eazybytes.cards.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cards")
+public class Cards {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long cardId;
+
+	private String mobileNumber;
+
+	private String cardNumber;
+
+	private String cardType;
+
+	private int totalLimit;
+
+	private int amountUsed;
+
+	private int availableAmount;
+
+	@Convert(converter = LocalDateTime.class)
+	private LocalDateTime createdAt;
+
+	@Column(updatable = false)
+	private String createdBy;
+
+	@Convert(converter = LocalDateTime.class)
+	@Column(insertable = false)
+	private LocalDateTime updatedAt;
+
+	@Column(insertable = false)
+	private String updatedBy;
+	
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	public Cards(Long cardId, String mobileNumber, String cardNumber, String cardType, int totalLimit, int amountUsed,
+			int availableAmount) {
+		super();
+		this.cardId = cardId;
+		this.mobileNumber = mobileNumber;
+		this.cardNumber = cardNumber;
+		this.cardType = cardType;
+		this.totalLimit = totalLimit;
+		this.amountUsed = amountUsed;
+		this.availableAmount = availableAmount;
+	}
+
+	public Cards() {
+		super();
+	}
+
+	public Long getCardId() {
+		return cardId;
+	}
+
+	public void setCardId(Long cardId) {
+		this.cardId = cardId;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public String getCardType() {
+		return cardType;
+	}
+
+	public void setCardType(String cardType) {
+		this.cardType = cardType;
+	}
+
+	public int getTotalLimit() {
+		return totalLimit;
+	}
+
+	public void setTotalLimit(int totalLimit) {
+		this.totalLimit = totalLimit;
+	}
+
+	public int getAmountUsed() {
+		return amountUsed;
+	}
+
+	public void setAmountUsed(int amountUsed) {
+		this.amountUsed = amountUsed;
+	}
+
+	public int getAvailableAmount() {
+		return availableAmount;
+	}
+
+	public void setAvailableAmount(int availableAmount) {
+		this.availableAmount = availableAmount;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+}
